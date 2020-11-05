9b3dcf6e2d849700123022e9043c162ea79e45f6
YeowHwee <yeowhwee13@gmail.com>
2020-11-05 18:04:50 +0800

    yes

---
+++ CodeMini2_xKYH/Assets/Scripts/MoveCubeZPosition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCubeZPosition : MonoBehaviour
+{
+    float speed;
+    float zLimit = 20.0f;
+    float xLimit = 0f;
+
+    bool isBouncing = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = Random.Range(2.0f, 10.0f);
+
+        //Step 1 of divide and conquer
+        if (transform.position.z < zLimit && isBouncing)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        else if (transform.position.z > -xLimit && !isBouncing)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * -speed);
+        }
+        else
+        {
+            isBouncing = !isBouncing;
+        }
+        
+    }
+    //Keeps the ball on the platform
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.parent = transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+
+}
---
+++ CodeMini2_xKYH/Assets/Scripts/PlayerController21.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController21 : MonoBehaviour
+{
+    float speed = 10.0f;
+    float gravityModifier = 2.0f;
+    bool isGrounded = true;
+
+    Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
